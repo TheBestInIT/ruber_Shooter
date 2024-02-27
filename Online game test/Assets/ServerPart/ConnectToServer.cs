fee8e26d80a9181fb35545e8aef5700f6b11c9b6
TheBestInIT <120022165+TheBestInIT@users.noreply.github.com>
2024-02-27 06:54:33 +0200

    gag
    gag

---
+++ Online game test/Assets/ServerPart/ConnectToServer.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("1_________________");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("2_________________");
+    
+    }
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+        Debug.Log("3_________________");
+
+    }
+
+
+}
+    
---
+++ Online game test/Assets/player/player_control/SpawnPlayers.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    [SerializeField] private Vector3 cordinate;
+    [SerializeField] private Vector3 cordinate2;
+    private int count;
+    private void Start()
+    {
+        count = PlayerPrefs.GetInt("AAA",0);
+        if (count == 0)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, cordinate, Quaternion.identity);
+            PlayerPrefs.SetInt("AAA",1);
+        }
+        if (count == 1)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, cordinate2, Quaternion.identity);
+            PlayerPrefs.SetInt("AAA",0);
+        }
+        PlayerPrefs.Save();
+        
+        
+        
+        Camera playerCamera = playerPrefab.GetComponentInChildren<Camera>();
+        Debug.Log(playerCamera.name);
+        playerCamera.gameObject.SetActive(false);
+      //  playerCamera.enabled = true;
+        
+        
+    }
+}
